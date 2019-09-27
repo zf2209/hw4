60cdb3409a0b1daae479493df251c658b90ae87f
kal <kal@kalx.net>
2019-09-27 16:56:43 -0400

    Sample test values.

--- xllblack/xll_black.cpp
+++ xllblack/xll_black.cpp
@@ -2,4 +2,7 @@
 
 //!!! Implement XLL.BLACK.PUT(f, sigma, k, t) and XLL.BLACK.CALL(f, sigma, k, t)
 //!!! Document both function.
-//!!! Test both function.
+//!!! Test both function. 
+//!!! put = 3.988 to three decimal places for f = 100, sigma = 0.2, k = 100, t = 0.25.
+//!!! Also test call value for these parameters.
+
